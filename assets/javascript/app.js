a14cb372bc0c311292d28576cc003c864f8dffde
austindickey <austindickey@me.com>
2020-01-21 21:03:19 -0600

    adding bad answers into the QandA object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,30 @@
 var QandA = [
     { q: "What’s the diameter of a basketball hoop in inches?", a: "18 inches", bad: "16 inches" },
-    { q: "What do you call it when a player makes three back to back strikes in bowling?", a: "Turkey" },
-    { q: "A sporting event is held every year on Memorial Day. What is it?", a: "Indianapolis 500" },
-    { q: "How many players are on a baseball team?", a: "9" },
-    { q: "Before 1894, basketball games were played with what sort of ball?", a: "Soccer Ball" },
-    { q: "What team gets the advantage of first bat in baseball?", a: "Away" },
-    { q: "How many dimples does an average golf ball have?", a: "336" },
-    { q: "What country has competed the most times in the summer Olympics without winning a gold medal?", a: "The Phillippines" },
-    { q: "How many minutes was the longest recorded point in the history of tennis?", a: "29" },
-    { q: "During the first ever modern Olympics, what were the first players awarded with?", a: "Silver medals" },
-    { q: "What is the only team in the NFL to neither host nor play in the Super Bowl?", a: "Cleveland Browns" },
-    { q: "Who was the youngest player to reach 10,000 points in the regular season in 20th century NBA?", a: "Kobe Bryant" },
-    { q: "How many seasons did Michael Jordan play with the Chicago Bulls?", a: "13" },
-    { q: "Kyrie Irving was born in what country?", a: "Australia" },
-    { q: "What is the NFL's record distance for a single punt?", a: "98 yards" }
+    { q: "What do you call it when a player makes three back to back strikes in bowling?", a: "Turkey", bad: "Trio" },
+    { q: "A sporting event is held every year on Memorial Day. What is it?", a: "Indianapolis 500", bad: "NBA Finals Game 1" },
+    { q: "How many players are on a baseball team?", a: "9", bad: "15" },
+    { q: "Before 1894, basketball games were played with what sort of ball?", a: "Soccer Ball", bad: "Volley Ball" },
+    { q: "What team gets the advantage of first bat in baseball?", a: "Away", bad: "Home" },
+    { q: "How many dimples does an average golf ball have?", a: "336", bad: "244" },
+    { q: "What country has competed the most times in the summer Olympics without winning a gold medal?", a: "The Phillippines", bad: "Iceland" },
+    { q: "How many minutes was the longest recorded point in the history of tennis?", a: "29", bad: "19" },
+    { q: "During the first ever modern Olympics, what were the first players awarded with?", a: "Silver medals", bad: "Platinum medals" },
+    { q: "What is the only team in the NFL to neither host nor play in the Super Bowl?", a: "Cleveland Browns", bad: "Jacksonville Jaguars" },
+    { q: "Who was the youngest player to reach 10,000 points in the regular season in 20th century NBA?", a: "Kobe Bryant", bad: "Lebron James" },
+    { q: "How many seasons did Michael Jordan play with the Chicago Bulls?", a: "13", bad: "11" },
+    { q: "Kyrie Irving was born in what country?", a: "Australia", bad: "Germany" },
+    { q: "What is the NFL's record distance for a single punt?", a: "98 yards", bad: "78 yards" }
 ]
 var beginning = true
 var userPoints = 0
+var questionIndex = 0
 
 $(document).ready(function () {
 
     function newQuestion() {
-        $("#question").text(QandA[0].q)
-        $("#answer1").append("<button>" + QandA[0].a + "</button>")
-        $("#answer2").append("<button>" + QandA[0].bad + "</button>")
+        $("#question").text(QandA[questionIndex].q)
+        $("#answer1").append("<button>" + QandA[questionIndex].a + "</button>")
+        $("#answer2").append("<button>" + QandA[questionIndex].bad + "</button>")
 
         $("#answer1").on("click", function () {
             $("#question").text("Correct!")
@@ -36,8 +37,9 @@ $(document).ready(function () {
             $("#question").text("Wrong!")
             $("#answer1").text("")
             $("#answer2").text("")
-            userPoints++
         })
+
+        questionIndex++
     }
 
     function startGame() {
